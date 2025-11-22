63460d45bfaec1cc2824263e84b33a1f22e42faa
Konstantin Zosimenko <pivasikkost@gmail.com>
2025-11-22 20:27:37 +0300

    update php version, add mysql db

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=localhost:3306;dbname=demo_project',
+    'username' => 'demo_user',
+    'password' => '123456',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
